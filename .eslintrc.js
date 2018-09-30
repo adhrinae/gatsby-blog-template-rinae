85ad177abce02681bf0f0f5d0d60c9f8e87d79d7
Ahn Dohyung <adhrinae@gmail.com>
2018-09-30 18:28:28 +0900

    Add eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['standard', 'standard-react'],
+  rules: {
+    'space-before-function-paren': 'off',
+    'jsx-quotes': ['error', 'prefer-double']
+  },
+  parser: 'babel-eslint'
+}
